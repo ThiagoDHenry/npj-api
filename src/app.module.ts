2866d0ecde74ae8ad3db730585e805fb60d11f48
Toxye1 <toxye2405@gmail.com>
2023-08-30 19:18:50 -0300

    att

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MotivoSchema } from './motivos/schema/motivo.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MotivoController } from './motivos/motivo.controller';
+import { MotivoService } from './motivos/shared/motivo.service';
+
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017:npj'),
+    MongooseModule.forFeature([{ name: 'Motivo', schema: MotivoSchema }]),
+],
+  controllers: [AppController, MotivoController],
+  providers: [AppService, MotivoService],
 })
 export class AppModule {}
---
+++ src/motivos/schema/motivo.schema.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const MotivoSchema = new mongoose.Schema({
+    descricao: String,
+    status: Boolean
+})
